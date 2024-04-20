16be5b3684a67261b6a9fb3734d4bfceaca6bae4
FrankoR2-D2 <frankoreynolds.fr@gmail.com>
2024-04-20 12:38:13 +0200

    Initial project setup, Added User, Post and Comment models

--- Models/Comments.cs
+++ Models/Comments.cs
@@ -1,6 +1,14 @@
 ﻿namespace RedditAPI.Models
 {
-    public class Comments
+    public class Comment
     {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; } // Reference to the Post the comment belongs to
+        public int? UserId { get; set; } // Optional user ID for commenting (can be null)
+        public User User { get; set; } // Optional reference to the User who created the comment (if implemented)
+        public DateTime CreatedAt { get; set; }
+        public int VoteCount { get; set; }
     }
 }
--- Models/Post.cs
+++ Models/Post.cs
@@ -2,5 +2,15 @@
 {
     public class Post
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } // Reference to the User who created the post
+        public DateTime CreatedAt { get; set; }
+        public int VoteCount { get; set; }
+
+        public ICollection<Comment> Comments { get; set; } // Navigation property for one-to-many relationship with Comment
     }
+}
 }
